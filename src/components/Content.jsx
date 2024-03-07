07bd7409c29fafef7fcba45e2f71b3176b995cea
Narges Ghanbari <narges.ghanbari@gmail.com>
2024-03-07 21:11:14 +0200

    Organize componts - part 2-1

---
+++ src/components/Content.jsx
@@ -0,0 +1,13 @@
+import Part from "./Part";
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part name={parts[0].name} count={parts[0].exercises} />
+      <Part name={parts[1].name} count={parts[1].exercises} />
+      <Part name={parts[2].name} count={parts[2].exercises} />
+    </div>
+  );
+};
+
+export default Content;
